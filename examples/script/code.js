ac85987526fb52bde863d2cd62ce7dabbb1b82de
gcusaaa <gcusaaa@gmail.com>
2023-11-07 16:18:21 +0200

    data type

---
+++ examples/script/code.js
@@ -0,0 +1,104 @@
+// named function 
+// function greeting() {
+//     console.log("Hello there");
+// }
+// calling a function
+
+// const btnSubmit =
+// document.getElementById('btnClick')
+// btnSubmit.addEventListener('click', greeting)
+
+// btnClick.addEventListener('click', greeting)
+
+// querySelector is highly recommended to keep track of your code and not 
+// const btnSubmit =
+// document.querySelector('#btnClick')
+// btnSubmit.addEventListener('click', greeting)
+
+// const btnSubmit =
+// document.querySelector('[data-submit]')
+// btnSubmit.addEventListener('click', greeting)
+
+
+// // Vari0ables
+// let firstName = 'Sipho'
+// console.log(firstName+' is working at Lifechoices')
+// console.log(firstName+' loves soccer')
+
+// let numb1 = 3
+// let numb2 = 4
+// console.log(numb1 + numb2)
+// console.log(numb1 * numb2)
+// console.log(numb1 - numb2)
+
+// primitive data type: String, number, boolean, indefined
+// dynamic data type: Array, object and function
+
+// let numb1 = 'Three'
+// console.log(numb1)
+// console.log(typeof numb1)
+
+// let numb1 = 'Three'
+// const isMale = true
+// console.log(numb1)
+// console.log(typeof numb1)
+// console.log(!isMale)
+// console.log(typeof numb1)
+
+// let numb1
+// let numb2 = null
+// console.log(numb1)
+// console.log(numb2)
+
+// initializing a variable
+// let numb1 = 6
+
+
+// const numb1;
+
+
+// let numb1
+// console.log(numb1)
+
+
+
+
+// let firstname = 'sipho'
+// let secondname = 'gcusa'
+// console.log(firstname + secondname)
+
+// let numberVar = 5
+// console.log(numberVar*5)
+
+// const name = sipho 
+// const name = gcusa
+
+
+// let numbers = 
+// connsole.table()
+
+let numb1 = "3"
+let numb2 = "4"
+console.log(
+    eval('${numb1} + ${numb1}'));
+console.log(parseInt(numb1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// greeting()
